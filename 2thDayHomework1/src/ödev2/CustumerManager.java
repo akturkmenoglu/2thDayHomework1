c150d0b2e89de6a09bb24bcfeb2ac9cb3d26c5fb
asus <asus@LAPTOP-663NJLTD>
2021-05-25 21:19:48 +0300

    2thDayHomework1

---
+++ 2thDayHomework1/src/ödev2/CustumerManager.java
@@ -0,0 +1,24 @@
+package ödev2;
+
+public class CustumerManager {
+
+	
+		public void Add() {
+			System.out.println("Müþteri eklendi");
+		}
+		
+		public void Remove() {
+			System.out.println("Müþteri silindi");
+		}
+		
+		public void Update() {
+			System.out.println("Müþteri Güncellendi");
+		}
+		
+		
+		
+		 
+		
+
+
+}
---
+++ 2thDayHomework1/src/ödev2/Mian.java
@@ -0,0 +1,14 @@
+package ödev2;
+
+public class Mian {
+
+	public static void main(String[] args) {
+		
+		CustumerManager custumerManager = new CustumerManager();
+		custumerManager.Add();
+		custumerManager.Remove();
+		custumerManager.Update();
+
+	}
+
+}
